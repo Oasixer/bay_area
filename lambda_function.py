d9d2111c932fd2249420279fb3730100fe5242c7
akmorrison <akmorris@edu.uwaterloo.ca>
2018-01-15 12:07:01 -0500

    Add additional error logging granularity, allow event to be a string

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
 from urlparse import parse_qs
 import logging
+import json
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -13,17 +14,46 @@ from wrt_respond import *
 
 def lambda_handler(event, context):
     try:
-        params = parse_qs(event['body'])
+        if type(event) == str:
+            params = parse_qs(json.loads(event)['body'])
+        else:
+            params = parse_qs(event['body'])
+    except:
+        logger.error('couldn\'t parse event{}'.format(event))
+        logger.error('error when parsing event["body"]')
+        return respond(None, "couldn't parse input")
+    try:
         user = params['user_id'][0]
+    except:
+        logger.error('couldn\'t parse event{}'.format(event))
+        logger.error('error when parsing user_id')
+        return respond(None, "couldn't parse input")
+    try:
         user_name = params['user_name'][0]
+    except:
+        logger.error('couldn\'t parse event{}'.format(event))
+        logger.error('error when parsing user_name')
+        return respond(None, "couldn't parse input")
+    try:
         command = params['command'][0]
+    except:
+        logger.error('couldn\'t parse event{}'.format(event))
+        logger.error('error when parsing command')
+        return respond(None, "couldn't parse input")
+    try:
         team_domain = params['team_domain'][0]
+    except:
+        logger.error('couldn\'t parse event{}'.format(event))
+        logger.error('error when parsing team_domain')
+        return respond(None, "couldn't parse input")
+    try:
         text = ""
         if "text" in params:
             text = params['text'][0]
         logger.info('%s (id %s) sent command %s to domain %s with text %s' % (user_name, user, command, team_domain, text))
     except:
         logger.error('couldn\'t parse event{}'.format(event))
+        logger.error('error when parsing text')
         return respond(None, "couldn't parse input")
     
     if command == '/bay':

6bd67b27c2991e02e502964cc6d6ec6a791fe771
akmorrison <akmorris@edu.uwaterloo.ca>
2018-02-22 18:06:48 -0500

    Add tags to todo items
    `/todo [TAG] todo item` now adds "todo item" to the TAG list of todo
    items, which can be viewed by all team members. All team members can
    also remove this todo item (this is changeable). `/todo clear` removes
    only untagged personal todo items, not items under tags.
    
    The goal of this is shared todo items. For example, if we have a list of
    stuff that needs to be done for the next static fire, Nick can do `/todo
    [SF1] machine the bulkhead`. Jacob can then do `/todo`, where he'll see
    the item, and he knows that he needs to machine the bulkhead. Once it's
    machined, he can remove this todo item with `/todo remove (number)`,
    with whatever number that todo item had.
    
    Also, Jacob machine the friggin bulkhead.

--- wrt_todo_command.py
+++ wrt_todo_command.py
@@ -1,5 +1,6 @@
 import time
 import decimal
+import re
 
 from wrt_respond import *
 from wrt_dynamodb_handler import *
@@ -12,6 +13,9 @@ def handle_todo_command(user, text, team_domain):
     elif text.startswith("remove") and len(text.split(' ')) == 2 and text.split(' ')[1].isdigit():
         return delete_todo_by_index(user, int(text.split(' ')[1]) - 1)
     else:
+        m = re.search(r"^\[([^\]]*)\](.*)$", text);
+        if m and len(m.group(1).strip()) > 0 and len(m.group(2).strip()) > 0:
+            return add_todo_item(user, m.group(2).strip(), m.group(1).strip());
         return add_todo_item(user, text)
 
 def get_all_todo_items_pretty(user):
@@ -19,28 +23,45 @@ def get_all_todo_items_pretty(user):
 
     items = {}
     for i in response:
-        items[str(float(i['time_added']))] = i['todo_item'].strip('"')
+        items[str(float(i['time_added']))] = (i['todo_item'].strip('"'), i['tag'] if 'tag' in i else 'personal')
     
-    sorted_items = []
+    sorted_items = {}
     for i in sorted(items.iterkeys()):
-        if items[i].startswith('[deleted]'):
+        if items[i][0].startswith('[deleted]'):
             continue
-        sorted_items.append(items[i])
+        if items[i][1] in sorted_items:
+            sorted_items[items[i][1]].append(items[i][0])
+        else:
+            sorted_items[items[i][1]] = [items[i][0]]
     if len(sorted_items) == 0:
         return respond(None, "You currently have no issues assigned to you. Enjoy your day!")
     return_string = "Here are your todo items:"
-    for i in range(len(sorted_items)):
-        return_string += "\n%i. %s" % (i + 1, sorted_items[i])
+    counter = 1
+    if 'personal' in sorted_items:
+        for i in sorted_items['personal']:
+            return_string += "\n%i. %s" % (counter, i)
+            counter += 1
+        sorted_items.pop('personal')
+    if sorted_items == {}:
+        return respond(None, return_string)
+
+    for i in sorted(sorted_items.iterkeys()):
+        return_string += "\n\n[%s]:" % i
+        for j in sorted_items[i]:
+            return_string += "\n%i. %s" % (counter, j)
+            counter += 1
     
     return respond(None, return_string)
 
 def get_all_todo_items(user):
-    return get_items_from_table('WRT_todo', lambda x: x['User_ID'] == user, 'todo_item')
+    return get_items_from_table('WRT_todo', lambda x: ('tag' in x and (x['tag'] != 'personal' or x['User_ID'] == user)) or ('tag' not in x and x['User_ID'] == user), 'todo_item')
 
 def clear_todo_items(user):
     items = get_all_todo_items(user)
     items_to_delete = {}
     for i in items:
+        if 'tag' in i and i['tag'] != 'personal':
+            continue
         items_to_delete[i['time_added']] = i['todo_item']
     if len(items_to_delete) > 0:
         deleted = delete_items_by_key(items_to_delete, "WRT_todo", "time_added", "todo_item")
@@ -48,25 +69,62 @@ def clear_todo_items(user):
     return respond(None, "no todo items to delete")
 
 def delete_todo_by_index(user, index):
-    items = get_all_todo_items(user)
-    items_map = {}
-    for i in items:
-        items_map[i['time_added']] = i['todo_item']
-        
-    if index < 0 or index >= len(sorted(items_map.iterkeys())):
-        return respond(None, "index out of range")
-    key = sorted(items_map.iterkeys())[index]
-    deleted = delete_items_by_key({key: items_map[key]}, "WRT_todo", "time_added", "todo_item")
-    return respond(None, "deleted %s items" % deleted)
+    response = get_all_todo_items(user)
 
-def add_todo_item(user, text):
+    items = {}
+    for i in response:
+        items[str(float(i['time_added']))] = (i['todo_item'], i['tag'] if 'tag' in i else 'personal', i['time_added'])
+
+    sorted_response = []
+    for i in sorted(items.iterkeys()):
+        if items[i][0].strip().startswith('[deleted]'):
+            continue
+        sorted_response.append({'todo_item':items[i][0],'tag':items[i][1], 'time_added':items[i][2]})
+
+    items_map = {}
+    for i in sorted_response:
+        if not 'tag' in i:
+            i['tag'] = 'personal'
+        if i['tag'] in items_map:
+            items_map[i['tag']].append( ( i['time_added'] , i['todo_item'] ) )
+        else:
+            items_map[i['tag']] = [ ( i['time_added'], i['todo_item']) ]
+        
+    if index < 0:
+        return respond(None, "positive indices only")
+
+    #first check personal
+    if 'personal' in items_map:
+        if index < len(items_map['personal']):
+            to_delete = items_map['personal'][index]
+            deleted = delete_items_by_key({to_delete[0]: to_delete[1]}, 'WRT_todo', 'time_added', 'todo_item')
+            return respond(None, "deleted item: %s" % to_delete[1])
+        index -= len(items_map['personal'])
+        items_map.pop('personal')
+
+    #then check alphabetically
+    for i in sorted(items_map.iterkeys()):
+        if index < len(items_map[i]):
+            to_delete = items_map[i][index]
+            deleted = delete_items_by_key({to_delete[0]: to_delete[1]}, 'WRT_todo', 'time_added', 'todo_item')
+            return respond(None, "deleted item: %s" % to_delete[1])
+        index -= len(items_map[i])
+        items_map.pop(i)
+
+    return respond(None, "index out of range")
+
+def add_todo_item(user, text, tag = None):
     item = text
     time_added = decimal.Decimal(time.time())
     Item = { 
         'time_added': time_added,
         'todo_item' : item,
-        'User_ID' : user
+        'User_ID'   : user,
+        'tag'       : tag if tag else 'personal'
     }
     put_item_in_table(Item, "WRT_todo")
     
-    return respond(None, "added: " + item)
+    if tag:
+        return respond(None, "added: " + item + " to [" + tag + "]")
+    else:
+        return respond(None, "added: " + item)
